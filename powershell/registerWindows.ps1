57c784bef677bdf196e4a2d07fc8119103800462
keroMimeo <47434421+keroMimeo@users.noreply.github.com>
2022-05-03 21:49:47 -0400

    Create registerWindows.ps1
    Register Windows using a PowerShell script

---
+++ powershell/registerWindows.ps1
@@ -0,0 +1,9 @@
+# From Robert Gullick's blog
+
+$computer = gc env:computername
+$key = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+$service = get-wmiObject -query "select * from SoftwareLicensingService" -computername $computer
+$service.InstallProductKey($key)
+$service.RefreshLicenseStatus()
+
+
